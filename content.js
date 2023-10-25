e6899395932b47b154fc3038926cdfbb558286e3
Alan Kistemaker <alan.kistemaker@modra.com>
2023-10-25 11:11:45 +1100

    init

---
+++ content.js
@@ -0,0 +1,9 @@
+// content.js
+
+// Function to change the background color
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = "black";
+}
+
+// Call the function when the page is loaded
+window.addEventListener("load", changeBackgroundColor);
