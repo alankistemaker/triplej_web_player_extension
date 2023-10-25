f0b92bd3ee87f3683b93e4d7771794b088e275bf
Alan Kistemaker <alan.kistemaker@modra.com>
2023-10-25 11:38:34 +1100

    removed fat arrow notation

--- content.js
+++ content.js
@@ -1,28 +1,28 @@
 // content.js
 
 // Function to change the background color
-const changeBackgroundColour = (colour) => {
+function changeBackgroundColour(colour) {
   document.body.style.backgroundColor = colour;
-};
+}
 
-const changeTextColour = (colour) => {
+function changeTextColour(colour) {
   document.body.style.color = colour;
-};
+}
 
-const changeElementTextColour = (element, colour) => {
+function changeElementTextColour(element, colour) {
   const elements = document.getElementsByClassName(element);
   for (let i = 0; i < elements.length; i++) {
     elements[i].style.color = colour;
   }
-};
+}
 
-const main = () => {
+function main() {
   changeBackgroundColor("black");
   changeTextColour("white");
   changeElementTextColour("Song_songArtist__sD5_H", "white");
   changeElementTextColour("Song_songRelease__jYe_C", "white");
   changeElementTextColour("Song_songWrapper__PySLU", "black");
-};
+}
 
 // Call the function when the page is loaded
 window.addEventListener("load", main);
