48f4e3ed879a650f193bcb29843de7ab606dd8a1
Alan Kistemaker <alan.kistemaker@modra.com>
2023-10-25 11:32:07 +1100

    added generic function

--- content.js
+++ content.js
@@ -5,13 +5,24 @@ function changeBackgroundColor() {
   document.body.style.backgroundColor = "black";
 }
 
-const changeBannerColour = () => {
-  const banner = document.getElementsByClassName("nav");
-  if (banner.length > 0) {
-    banner[0].style.backgroundColor = "black";
+const changeTextColour = () => {
+  document.body.style.color = "white";
+};
+
+const changeElementTextColour = (element, colour) => {
+  const elements = document.getElementsByClassName(element);
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.color = colour;
   }
 };
 
+const main = () => {
+  changeBackgroundColor();
+  changeTextColour();
+  changeElementTextColour("Song_songArtist__sD5_H", "white");
+  changeElementTextColour("Song_songRelease__jYe_C", "white");
+  changeElementTextColour("Song_songWrapper__PySLU", "black");
+};
+
 // Call the function when the page is loaded
-window.addEventListener("load", changeBackgroundColor);
-window.addEventListener("load", changeBannerColour);
+window.addEventListener("load", main);
